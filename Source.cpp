f44220c8231ea7a524a32e0365f852053fe4d148
kenjinote <2605401+kenjinote@users.noreply.github.com>
2026-03-23 03:17:18 +0900

    バージョン情報を更新

--- Source.cpp
+++ Source.cpp
@@ -304,7 +304,7 @@ ParsedPage FetchAndStripHTML(const std::string& url, const std::string& postData
             L"<b>Ctrl + C</b> : 選択中のテキストをクリップボードにコピー<br>"
             L"<b>q</b> : ブラウザを終了する<br><br>"
             L"<hr><br>"
-            L"t2t(v1.0.2)"
+            L"t2t(v1.0.3)"
             L"</body></html>";
     }
     else {
